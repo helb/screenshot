24a2834c1f2276f2569d62c047a7232bc7f22d46
helb <helb@helb.cz>
2017-03-06 09:51:45 +0100

    update for compatibility with upstream maim

--- screenshot3.sh
+++ screenshot3.sh
@@ -1,5 +1,5 @@
 #!/bin/sh            
-SCREENSHOT_DIR="$HOME/.screenshot"
+SCREENSHOT_DIR="$HOME/.local/screenshot"
 
 if [ ! -x ${SCREENSHOT_DIR} ]; then mkdir -p ${SCREENSHOT_DIR}; fi
 
@@ -7,8 +7,8 @@ SCREENSHOT_NAME=`hostname`"_"`date +%Y%m%d_%H%M%S.png`
 
 if pgrep -x "compton" > /dev/null
 then
-    OPENGL="--opengl"
+    OPENGL="-o"
 fi
 
-maim -s --showcursor -b 1 ${OPENGL} ${SCREENSHOT_DIR}/${SCREENSHOT_NAME}
+maim -s -b 1 ${OPENGL} ${SCREENSHOT_DIR}/${SCREENSHOT_NAME}
 notify-send "Screenshot ${SCREENSHOT_NAME} uložen do ${SCREENSHOT_DIR}."
